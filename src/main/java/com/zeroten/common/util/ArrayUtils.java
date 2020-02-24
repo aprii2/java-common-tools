e7ff9fde521eed67a49b4752be52ee89cddbe6bd
panyun01_sx <panyun01_sx@qiyi.com>
2020-02-24 16:56:22 +0800

    Add SortMethod for ArrayUtils

--- src/main/java/com/zeroten/common/util/ArrayUtils.java
+++ src/main/java/com/zeroten/common/util/ArrayUtils.java
@@ -1,4 +1,18 @@
 package com.zeroten.common.util;
 
 public class ArrayUtils {
+    // 1、对传入的 int 数组进行排序，从小到大。
+    // 2、并返回排序后的结果。
+    public static int[] sort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i ++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
--- src/test/java/com/zeroten/common/util/ArrayUtilsTest.java
+++ src/test/java/com/zeroten/common/util/ArrayUtilsTest.java
@@ -1,4 +1,15 @@
 package com.zeroten.common.util;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ArrayUtilsTest {
+
+    @Test
+    public void testSort() {
+        //对传入的 int 数组进行排序，从小到大，并返回排序后的结果。
+        int[] arr1 = new int[]{5,43,23,1,17};
+        int[] ans1 = new int[]{1,5,17,23,43};
+        Assert.assertEquals(ArrayUtils.sort(arr1),ans1);
+    }
 }
